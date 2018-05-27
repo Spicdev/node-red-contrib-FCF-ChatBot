2ef7089bc9b803f3203c13a32915e45f6401b56c
XanxusVer.V.R <xanxusver.v.r@gmail.com>
2018-05-27 15:57:36 +0800

    改為Promise寫法

--- FCF-Dispatcher.js
+++ FCF-Dispatcher.js
@@ -1,7 +1,8 @@
 var request = require("request");
-var crypto = require("crypto");//引用可以產生亂數字串的模組
+var crypto = require("crypto"); //引用可以產生亂數字串的模組
+const { GoogleToken } = require("gtoken");
 
-module.exports = function(RED) {
+module.exports = function (RED) {
     function Dispatcher(config) {
 
         RED.nodes.createNode(this, config);
@@ -9,50 +10,65 @@ module.exports = function(RED) {
         node.token = config.token;
         node.rules = config.rules;
 
-        this.on("input", function(msg) {
+        this.on("input", function (msg) {
 
             var rules = node.rules;
-            var token = encodeURIComponent(node.token);
+            // var token = encodeURIComponent(node.token);
             var output = [];
             var matched = false;
-            var buf = crypto.randomBytes(30);//產生一個30byte的亂數資料，來當作請求網址的session ID
+            var buf = crypto.randomBytes(25); //產生一個30byte的亂數資料，來當作請求網址的session ID
 
-            var headers = {
-                "Content-Type": "application/json;charset=utf-8",
-                "Authorization": "Bearer " + token,
-            }
-
-            var options = {
-                url: `https://dialogflow.googleapis.com/v2/projects/parkingbot-c50be/agent/sessions/${buf.toString("base64")}:detectIntent`,
-                method: "POST",
-                headers: headers,
-                body: JSON.stringify({
-                    "queryInput": {
-                        "text": {
-                            "text": msg.payload.content,
-                            "languageCode": "zh-TW"
-                        }
-                    }
-                })
-            }
-
-            request(options, function(error, response, body) {
-                var body = JSON.parse(body);
-                var intent = body.queryResult.intent.displayName;
-                rules.forEach(function(rule){
-                    if(intent == (rule.topic).toString()){
-                        matched = true;
-                        if(intent == "Default Fallback Intent" ){
-                            msg.payload = body.queryResult.queryText;
-                        }
-                        output.push(msg);
-                    }
-                    else{
-                        output.push(null);
-                    }
-                });
-                node.send(output);
+            const gtoken = new GoogleToken({
+                keyFile: "./ParkingBot-5ca59ac9f58a.json",
+                scope: ["https://www.googleapis.com/auth/cloud-platform"] // or space-delimited string of scopes
             });
+            var sendRequest = function (token) {
+
+                var headers = {
+                    "Content-Type": "application/json;charset=utf-8",
+                    "Authorization": "Bearer " + token,
+                }
+
+                var options = {
+                    url: `https://dialogflow.googleapis.com/v2/projects/parkingbot-c50be/agent/sessions/${buf.toString("hex")}:detectIntent`,
+                    method: "POST",
+                    headers: headers,
+                    body: JSON.stringify({
+                        "queryInput": {
+                            "text": {
+                                "text": msg.payload.content,
+                                "languageCode": "zh-TW"
+                            }
+                        }
+                    })
+                }
+
+                request(options, function (error, response, body) {
+                    var body = JSON.parse(body);
+                    var intent = body.queryResult.intent.displayName;
+                    rules.forEach(function (rule) {
+                        if (intent == (rule.topic).toString()) {
+                            matched = true;
+                            if (intent == "Default Fallback Intent") {
+                                msg.payload = body.queryResult.queryText;
+                            }
+                            output.push(msg);
+                        } else {
+                            output.push(null);
+                        }
+                    });
+                    node.send(output);
+                });
+            }
+
+            var gt = function () {
+                gtoken.getToken().then(function (token) {
+                    return sendRequest(token);
+                }).catch(function (error) {
+                    console.log(error)
+                });
+            }
+            gt();
         });
     }
     RED.nodes.registerType("FCF-Dispatcher", Dispatcher);
